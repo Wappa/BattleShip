66ee8864b54f00a7b7ec274701c12833bd0dcec7
Wappa <aurelien.isak@epita.fr>
2020-06-20 17:00:29 +0200

    Finish Add Matrix Specification

--- BattleShip/BattleShip/Matrix.cs
+++ BattleShip/BattleShip/Matrix.cs
@@ -32,22 +32,60 @@ namespace BattleShip
                     invalidDes = true;
                     nbDestroyer += 1;
                 }
-                Console.WriteLine("Coordonnée valide, Choisissez la direction de votre bateau \n 0 = North , 1 = South , 2 = West and 3= East ");
-                string Direction = Console.ReadLine();
-                switch (Direction)
-                { 
-                    case "0":
-                        matrice[UnCoor, SeCoor] = new Ship(Ship.Types.DESTROYER,Ship.Directions.NORTH);
-                        break;
-                    case "1":
-                        matrice[UnCoor, SeCoor] = new Ship(Ship.Types.DESTROYER,Ship.Directions.SOUTH);
-                        break;
-                    case "2":
-                        matrice[UnCoor, SeCoor] = new Ship(Ship.Types.DESTROYER,Ship.Directions.WEST);
-                        break;
-                    case "3":
-                        matrice[UnCoor, SeCoor] = new Ship(Ship.Types.DESTROYER,Ship.Directions.EAST);
-                        break;
+
+                bool ExactD = true;
+
+                while (ExactD)
+                {
+                    Console.WriteLine("Coordonnée valide, Choisissez la direction de votre bateau \n 0 = North , 1 = South , 2 = West and 3= East ");
+                    string Direction = Console.ReadLine();
+                    
+                    switch (Direction)
+                    { 
+                        case "0":
+                            if (UnCoor-4 <= 0)
+                            {
+                                Console.WriteLine("Invalid position try again");
+                                ExactD = true;
+                            }
+                            else
+                                matrice[UnCoor, SeCoor] = new Ship(Ship.Types.DESTROYER,Ship.Directions.NORTH);
+
+                            ExactD = false;
+                            break;
+                        case "1":
+                            if (UnCoor +4 <= lenMat)
+                            {
+                                Console.WriteLine("Invalid position try again");
+                                ExactD = true;
+                            }
+                            else
+                                matrice[UnCoor, SeCoor] = new Ship(Ship.Types.DESTROYER,Ship.Directions.SOUTH);
+
+                            ExactD = false;
+                            break;
+                        case "2":
+                            if (SeCoor - 4 <= 0)
+                            {
+                                Console.WriteLine("Invalid position try again");
+                                ExactD = true;
+                            }
+                            else
+                                matrice[UnCoor, SeCoor] = new Ship(Ship.Types.DESTROYER,Ship.Directions.WEST);
+
+                            ExactD = false;
+                            break;
+                        case "3":
+                            if (SeCoor + 4 > lenMat)
+                            {
+                                Console.WriteLine("Invalid position try again");
+                            }
+                            else
+                                matrice[UnCoor, SeCoor] = new Ship(Ship.Types.DESTROYER,Ship.Directions.EAST);
+
+                            ExactD = false;
+                            break;
+                    }
                 }
             }
             while (nbCruiser < 1 || invalidCru == false)
@@ -74,7 +112,7 @@ namespace BattleShip
                     switch (Direction)
                     {
                         case "0":
-                            if (UnCoor - 2 < 0)
+                            if (UnCoor - 3 < 0)
                             {
                                 Console.WriteLine("Invalid Position try again");
                                 ExactC = true;
@@ -84,7 +122,7 @@ namespace BattleShip
 
                             break;
                         case "1":
-                            if (UnCoor + 3 > matrice.Length || SeCoor + 3> matrice.Length)
+                            if (UnCoor + 3 > matrice.Length)
                             {
                                 Console.WriteLine("Invalid Position try again ");
                                 ExactC = true;
@@ -96,24 +134,24 @@ namespace BattleShip
                             break;
 
                         case "2":
-                            if (SeCoor - 2 <= 0)
+                            if (SeCoor - 3 <= 0)
                             {
                                 Console.WriteLine("Invalid Position try again");
                                 ExactC = true;
                             }
                             else
-                                matrice[UnCoor, SeCoor] = new Ship(Ship.Types.MINIDICK, Ship.Directions.WEST);
+                                matrice[UnCoor, SeCoor] = new Ship(Ship.Types.CRUISER, Ship.Directions.WEST);
 
                             ExactC = false;
                             break;
                         case "3":
-                            if (SeCoor + 2 >= lenMat)
+                            if (SeCoor + 3 >= lenMat)
                             {
                                 Console.WriteLine("Invalid Position try again");
                                 ExactC = true;
                             }
                             else
-                                matrice[UnCoor, SeCoor] = new Ship(Ship.Types.MINIDICK, Ship.Directions.EAST);
+                                matrice[UnCoor, SeCoor] = new Ship(Ship.Types.CRUISER, Ship.Directions.EAST);
 
                             ExactC = false;
                             break;
